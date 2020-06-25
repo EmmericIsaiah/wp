f9209ab0bf0713f7dd845147dbde8b7171228e7e
EmmericIsaiah <47565274+EmmericIsaiah@users.noreply.github.com>
2020-06-25 13:39:23 +0200

    new footer sidebar form

---
+++ js/backup-prov-apps.js
@@ -0,0 +1,68 @@
+( function() {
+    var nav = document.getElementById( 'site-navigation' ), button, menu;
+    if ( ! nav ) {
+        return;
+    }
+
+    button = nav.getElementsByTagName( 'button' )[0];
+    menu   = nav.getElementsByTagName( 'ul' )[0];
+    if ( ! button ) {
+        return;
+    }
+
+    // Hide button if menu is missing or empty.
+    if ( ! menu || ! menu.childNodes.length ) {
+        button.style.display = 'none';
+        return;
+    }
+
+    button.onclick = function() {
+        if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+            menu.className = 'nav-menu';
+        }
+
+        if ( -1 !== button.className.indexOf( 'toggled-on' ) ) {
+            button.className = button.className.replace( ' toggled-on', '' );
+            menu.className = menu.className.replace( ' toggled-on', '' );
+        } else {
+            button.className += ' toggled-on';
+            menu.className += ' toggled-on';
+        }
+    };
+} )(jQuery);
+
+
+
+jQuery(document).ready(function($) {
+
+});
+
+
+jQuery(document).ready(function ($) {
+    // with jQuery
+    var container = jQuery('#grid-container');
+    const backup_menu_item = jQuery('.menu-item-has-children').clone();
+
+    // initialize Masonry after all images have loaded
+    container.imagesLoaded( function() {
+        container.masonry();
+    });
+
+    // This will fire when document is ready:
+    $(window).resize(function() {
+        // This will fire each time the window is resized:
+        if($(window).width() <= 800 ) { //C'est ici pour fa ire disparaître le menu down en dessus de 800px de large
+            jQuery('li.menu-item-has-children>ul').prepend('<button  class="submenu-button">DOWN</button>');
+            jQuery('.submenu-button').siblings().hide();
+            jQuery('.submenu-button').click( function(){
+                jQuery(this).siblings().toggle();
+            });
+            jQuery('.menu-item-has-children').remove();
+        }
+        else {
+            jQuery('.menu-item-has-children').replaceWith(backup_menu_item) ;
+            jQuery('.submenu-button').siblings().show();
+        }
+    }).resize(); // This will simulate a resize to trigger the initial run.
+
+});
